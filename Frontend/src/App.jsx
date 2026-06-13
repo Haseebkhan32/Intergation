8c651667d77dc2d79fee2527d8c0bcfa331d61b7
Haseebkhan32 <haseebkhanb321@gmail.com>
2026-06-13 10:44:46 +0500

    Live Code Push

--- Frontend/src/App.jsx
+++ Frontend/src/App.jsx
@@ -1,12 +1,17 @@
 import React from 'react'
 import { Route, Router, Routes } from 'react-router-dom'
 import {Login , SignUp } from './Pages/'
+import Layout from '../src/component/layout';
+
 
 const App = () => {
   return (
    <Routes>
-    <Route path='/' element={<Login/>}/>
+    <Route path='/login' element={<Login/>}/>
     <Route path='/signup' element={<SignUp/>}/>
+    <Route path='/' element={<Layout/>} >
+
+    </Route>
    </Routes>
   )
 }
